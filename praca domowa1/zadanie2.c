823044291268bc3d38803d25e871312f6c2c270b
fofre1 <77551827+fofre1@users.noreply.github.com>
2024-11-13 19:43:52 +0100

    Add files via upload

---
+++ praca domowa1/zadanie2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+int fibonaci[100], ilosc_liczb_ciagu;
+fibonaci[0]=0;
+fibonaci[1]=fibonaci[2]=1;
+printf("Ile liczb ciagu fibonaciego chesz wyświetlic?: ");
+scanf(%i, &ilosc_liczb_ciagu);
+printf("\n %i", ilosc_liczb_ciagu);
+
+
+
+}
