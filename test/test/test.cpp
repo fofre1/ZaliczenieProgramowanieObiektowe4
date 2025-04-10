0d13ef6fbe0139d83b4f4659d89604c74005b82d
fofre1 <77551827+fofre1@users.noreply.github.com>
2025-04-10 17:16:37 +0200

    Add files via upload

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -1,5 +1,16 @@
 ﻿// test.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
 //
+//Itemy liczba co znaczy:
+// 1 - weapon
+// 2 - armor
+// 3 - head_armor
+// 4 - chestplate
+// 5 - left hand
+// 6 - right hand
+// 7 - leggins
+// 8 - left boot
+// 9 - right boot
+//
 
 #include <iostream>
 #include <ctime>
@@ -7,7 +18,17 @@
 #include <cmath>
 #include <stdlib.h>
 
-struct special_item {
+struct item {
+    char name[50];
+    int dmg;
+    float penetracja;
+    int wytrzymalosc;
+    float weight;
+    int slots;
+    int hand_slot;
+    int hp;
+    float resist;
+    int typeOfItem;
     int fire_attack;
     int poison_attack;
     int stun_attack;
@@ -16,33 +37,16 @@ struct special_item {
     int dodge_modifier;
 };
 
-struct weapon {
-    char name[50];
-    int dmg;
-    float penetracja;
-    int wytrzymalosc;
-    float weight;
-    int slots;
-    int hand_slot;
-    special_item modifier1;
-    special_item modifier2;
-};
-
-struct armor {
-    int hp;
-    float resist;
-    special_item modifier1;
-    special_item modifier2;
+struct backpack2 {
+    item* plecak_item;
+    int weight;
+    int current_item_slot = 0;
 };
 
 struct backpack {
-    int size = 50;
-    special_item* plecak_special_items = (special_item*)malloc(sizeof(special_item) * size);
-    armor* plecak_armor = (armor*)malloc(sizeof(armor) * size);
-    weapon* plecak_miecz = (weapon*)malloc(sizeof(weapon) * size);
+    item* plecak_item;
     int weight;
-    int current_weapon_slot=0;
- 
+    int current_item_slot=0;
 };
 
 struct champ{
@@ -56,122 +60,118 @@ struct champ{
     int poison_resistance;
     int fire_resistance;
     float dodge;
-    int free_left_hand_slot;
-    int free_right_hand_slot;
-    weapon* right_hand;
-    weapon* left_hand;
-    armor* head;
-    armor* chestplate;
+    int inv_slots[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+    item* right_hand;
+    item* left_hand;
+    item* head;
+    item* chestplate;
+    item* leggins;
+    item* left_boot;
+    item* right_boot;
     backpack* plecak;
+    backpack2** plecak2;
 };
 
+champ generate(champ* current, int plecak_size, int plecak_rows, int plecak_collums) {
 
-
-
-champ generate(champ current) {
-    current.hp = (15 + rand() % 45);
-    current.resist = (1 + rand() % 3);
-    current.sila= (1 + rand() % 4);
-    current.free_left_hand_slot = 0;
-    current.free_right_hand_slot = 0;
-    return current;
+    current->hp = (15 + rand() % 45) ;
+    current->resist = (1 + rand() % 3);
+    current->sila= (1 + rand() % 4);
+    current->plecak = (backpack*)malloc(sizeof(backpack) * plecak_size);
+    current->plecak2 = (backpack2**)malloc(sizeof(backpack*) * plecak_rows);
+    for (int i = 0; i < plecak_rows; i++) {
+        current->plecak2[i] = (backpack2*)malloc(sizeof(backpack2) * plecak_collums);
+    }
+    return *current;
 }
 
-special_item generate(special_item current) {
-    int los1 = rand() % 5, los2 = rand() % 15, los3 = rand()%160;
-    if (los1 == 1) {
-        current.fire_attack = 1;
-    }
-    if (los1 == 2) {
-        current.poison_attack = 1;
-    }
-    if (los2 = 10) {
-        current.stun_attack = 1;
-    }
-    if (los1 == 3 && los2 == 1)
-    {
-        current.fire_attack = 2;
-        current.stun_attack = 1;
-    }
-    if (los1 == 3 && los2 == 11)
-    {
-        current.poison_attack = 2;
-        current.stun_attack = 1;
-    }
-    if (los1 == 3 && los2 == 5)
-    {
-        current.poison_attack = 2;
-        current.fire_attack = 2;
-    }
-
-    if (los3 == 1)
-    {
-        current.stun_attack = 2;
-        current.poison_attack = 1;
-        current.fire_attack = 1;
-    }
-
-    if (los1 == 1 && los2 == 2 && los3 == 3)
-    {
-        current.stun_attack = 4;
-        current.poison_attack = 4;
-        current.fire_attack = 4;
-    }
-
-
-    return current;
-}
-
-weapon generate(weapon current) {
-    current.dmg = (1 + rand() % 6);
-    current.penetracja = (100 + rand() % 60)/100.0;
-    current.wytrzymalosc = (169 + rand() % 345);
-    current.weight = (2 + rand() % 6) / 2.0;
-    current.hand_slot = (1 + rand() % 2);
-    current.slots = 0;
+item generate(item* current) {
+    current->dmg = (1 + rand() % 6);
+    current->penetracja = (100 + rand() % 60)/100.0;
+    current->wytrzymalosc = (169 + rand() % 345);
+    current->weight = (2 + rand() % 6) / 2.0;
+    current->hand_slot = (1 + rand() % 2);
+    current->typeOfItem = 1;
+    current->slots = 0;
     if ((1 + rand() % 5) == 1)
     {
-        current.slots++;
+        current->slots++;
         if ((1 + rand() % 15) == 1)
         {
-            current.slots++;
+            current->slots++;
         }
     }
-    return current;
+    current->hp = (58 + rand() % 129);
+    current->resist = (1 + rand() % 7);
+    //current->typeOfItem = (1 + rand % 9); // Losowanie do jakiego slota wchodzi item, patrz samą górę, linijke 1 ^
+    return *current;
 }
 
-armor generate(armor current) {
-    current.hp = (58 + rand() % 129);
-    current.resist = (1 + rand() % 7);
-    return current;
-}
 
-float calc_hit_point(weapon bron, champ postac, champ przeciwnik)
+/*float calc_hit_point(weapon bron, champ postac, champ przeciwnik)
 {
     float hit_point = (pow(log(bron.dmg), 2) + 3) * 2 * bron.penetracja * przeciwnik.sila / postac.resist;
     return hit_point;
-}
+}*/
 
-void add_item_weapon(champ* postac, weapon* current_item) {
-    printf("1");
-    if (current_item->hand_slot == 1 && postac->free_left_hand_slot == 0) {
-        postac->left_hand = current_item;
-        postac->free_left_hand_slot++;
-        printf("a");
-    }
-    else if(current_item->hand_slot == 2 && postac->free_right_hand_slot==0){
-        postac->right_hand = current_item;
-        postac->free_right_hand_slot++;
-        printf("b");
+void add_item(champ* postac, item* current_item) {
+    printf("%i", current_item->typeOfItem);
+    if (postac->inv_slots[current_item->typeOfItem - 1] == 0) {
+        switch (current_item->typeOfItem) {
+        case 1:
+            postac->left_hand = current_item;
+            printf("a");
+            break;
+        case 2:
+            postac->right_hand = current_item;
+            printf("b");
+            break;
+        case 3:
+            postac->head = current_item;
+            printf("c");
+            break;
+        case 4:
+            postac->chestplate = current_item;
+            printf("d");
+            break;
+        case 5:
+            postac->left_hand = current_item;
+            printf("e");
+            break;
+        case 6:
+            postac->right_hand = current_item;
+            printf("f");
+            break;
+        case 7:
+            postac->leggins = current_item;
+            printf("g");
+            break;
+        case 8:
+            postac->left_boot = current_item;
+            printf("h");
+            break;
+        case 9:
+            postac->right_boot = current_item;
+            printf("i");
+            break;
+        default:
+            printf("j");
+            break;
+        }
+        postac->inv_slots[current_item->typeOfItem - 1]++;
     }
     else {
+        printf("Slot jest zajety");
     }
-    printf("2");
 }
 
-void add_weapon_backpack(champ* postac, weapon* bron) {
-    postac->plecak->plecak_miecz[postac->plecak->current_weapon_slot] = bron;
-    postac->plecak->current_weapon_slot++;
+/*void add_item_to_backpack(champ* postac, item* przedmiot) {
+    postac->plecak->plecak_item[postac->plecak->current_item_slot] = przedmiot;
+    postac->plecak->current_item_slot++;
+}*/
+
+void add_item_to_backpack2(champ* postac, item* przedmiot, int posX, int posY) {
+    postac->plecak2[posX][posY].plecak_item = przedmiot;
 }
 
 /*void walka(champ postac, champ przeciwnik, weapon bron_champ, weapon bron_przeciwnik) {
@@ -205,22 +205,45 @@ void add_weapon_backpack(champ* postac, weapon* bron) {
 
 int main()
 {
+    int plecak_size = 50;
+    int plecak_collumns = 10, plecak_rows = 10, posX=1, posY=1;
     srand(time(NULL));
     backpack playerBackpack;
-    weapon miecz = { 0,0 }, miecz2 = { 0,0 }, miecz3 = { 0,0 };
-    special_item wzmocnienie = { 0,0 };
-    champ postac = { 0,0 }, przeciwnik = { 0,0 };
-    miecz=generate(miecz);
-    miecz2 = generate(miecz2);
-    miecz3 = generate(miecz3);
-    wzmocnienie = generate(wzmocnienie);
-    postac = generate(postac);
-    add_item_weapon(&postac, &miecz);
+    item *przedmiot = (item*)malloc(sizeof(item)), *przedmiot2 = (item*)malloc(sizeof(item)), *przedmiot3 = (item*)malloc(sizeof(item));
+    champ* postac = (champ*)malloc(sizeof(champ));
+    generate(postac, plecak_size, plecak_rows, plecak_collumns);
+    generate(przedmiot);
+    add_item(postac, przedmiot);
+    //add_item_to_backpack2(postac, przedmiot, posX, posY);
+    //posX++;
+    //posY++;
+    generate(przedmiot2);
+    add_item(postac, przedmiot2);
+    //add_item_to_backpack2(postac, przedmiot2, posX, posY);
+    //posX++;
+    //posY++;
+    generate(przedmiot3);
+    add_item(postac, przedmiot3);
+    //add_item_to_backpack2(postac, przedmiot3, posX, posY);
+
+    /*add_item_weapon(postac, przedmiot);
+    add_item_weapon(postac, przedmiot2);
+    add_item_weapon(postac, przedmiot3);*/
+
+    //add_item_weapon(&postac, &miecz);
     /*postac.hp += 15;
     postac.armor += 3;
     postac.sila += 3;
     przeciwnik = generate(przeciwnik);
     walka(postac, przeciwnik, miecz, miecz2);*/
+    free(przedmiot);
+    free(przedmiot2);
+    free(przedmiot3);
+    for (int i = 0; i < plecak_collumns; i++)
+        if(postac->plecak2[i]!=NULL) free(postac->plecak2[i]);
+    free(postac->plecak2);
+    free(postac);
+
 }
 
 // Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
